b17623a4b24af4ee0c8be61cd5b16b65e9884c18
kbalyanov <kbalyanov@yahoo.com>
2021-04-16 10:47:03 +0300

    wsfrew

--- js/lesson4hw.js
+++ js/lesson4hw.js
@@ -1,31 +1,38 @@
 'use strict'
-let num = [];
-function count(num,text=""){
+
+function count(num){
    let prmore = "товара"
    let prles = "товаров"
    let prelse = "товар"
-   if(num % 10 ===3) return text + prmore 
-   else if(num % 10 ===5) return text + prles 
-   else if(num % 10 ===2) return text + prmore
-   else if(num % 10 ===4) return text + prmore
-   else if(num % 10 ===6) return text + prles
-   else if(num % 10 ===7) return text + prles
-   else if(num % 10 ===8) return text + prles
-   else if(num % 10 ===9) return text + prles
-   else return text + prelse
+   if(num % 10 ===5) return num + prles 
+   else if(num % 10 >=2 && num % 10 <= 4) return num + prmore 
+   else if(num % 10 >=6 &&  num % 10 <= 9) return num + prles
+   else return num + prelse
 }
-let hey = count(1,"1");
+let hey = count(7);
 console.log(hey)
 
-let startUp = []
+let startUp = [];
 function range(start, end, step=1){
-    
-  for (let i = range.length -1; i >=0; i -= 1) {
-    if (range[i] % 2 !==0){
-        // range.splice(i, 1) 
-    }   return step, end,start;
+    let val = []
+    for (let i = start; i <= end; i +=step){
+    val.push.apply(startUp=i);
+    val += 1;
+    }
+    return startUp;
 }
+console.log(range(4,2,7))
 
-}
-let llc = range(3,2,1)
-console.log(llc)
+// function checkSpam(text,...words)
+//     let sttext = text.split('');
+//     let numbe = 0;
+//     for(let i =0; i < sttext.length; i++){
+
+//     }
+
+
+
+
+
+
+
