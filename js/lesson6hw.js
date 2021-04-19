9ef5e99370ec2a5c78e015c61c173fbea5c6ccc1
kbalyanov <kbalyanov@yahoo.com>
2021-04-19 22:50:42 +0300

    leshw6

--- js/lesson6hw.js
+++ js/lesson6hw.js
@@ -10,12 +10,28 @@
 // console.log(words)
 
 let str = "дом встречается в строке дом домик домой одомашненный";
+
+function sameword(){
 let count = 0;
 let text = str.indexOf('дом');
-
 while (text !== -1) {
   count++;
   text = str.indexOf('дом', text + 1);
 }
+return count;
 
-console.log(count); 
+}
+console.log(sameword(str))
+
+// let pal = "";
+// let text = pal.slice
+
+function list() {
+    return Array.prototype.slice.call(arguments, 0);
+  }
+  
+  let list1 = list(1, 2, 3); 
+
+  console.log(list1)
+
+
